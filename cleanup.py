5858637b8ac923c435e96fc6406da4c324dbe5da
shippj <github@nospam.jshipp.com>
2020-04-28 13:06:08 -0500

    Add files via upload

---
+++ cleanup.py
@@ -0,0 +1,64 @@
+import os
+import mysql.connector
+import sys
+#import datetime
+
+print("stopping sniffer service...")
+os.system("systemctl stop sniffer")
+
+print("Connecting to DB...\n")
+mysql = mysql.connector.connect(
+        host="127.0.0.1",
+        user="sniffer",
+        password="sniffer",
+        database="traffic"
+    );
+print (mysql)
+
+print("preparing sql cursor...")
+c = mysql.cursor()
+
+#print("finding min/max ID..")
+#c.execute("SELECT min(id) as min, max(id) as max  FROM data")
+#r = c.fetchone()
+#print(r)
+
+#min = r[0]
+#max = r[1]
+
+#min = max - 100000000;
+
+#print("deleting ID < " + str(min))
+#c.execute("delete from data where id < " + str(min));
+#r = c.fetchone()
+
+#print("optimizing table...")
+#c.execute("optimize table data")
+#r = c.fetchone()
+
+print("Finding DAY... ")
+c.execute("select dayofyear(curdate()-1) as day")
+r = c.fetchone()
+day = r[0]
+print(" day = " + str(day) )
+
+print("Checking to make sure we dont already have reports for this day...")
+c.execute("select count(*) from reports where day="+str(day))
+r = c.fetchone()
+if (r[0]>0) :
+	print(" WE ALREADY DONE THIS REPORT!  ABORTING!")
+	sys.exit(0)
+
+print("Calculating new reports...")
+c.execute("insert into reports select day, from_ip, to_ip, sum(size), count(*) from data where day = " + str(day) + " group by day, from_ip, to_ip");
+r = c.fetchone()
+
+print("Deleting data table...")
+c.execute("delete from data")
+r = c.fetchone()
+
+print("Starting sniffer service...")
+os.system("systemctl start sniffer")
+
+print("Done!")
+
---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>JShipp Traffic Recorder</title>
+  </head>
+  <body>
+	<?php
+	
+	
+	
+$servername = "localhost";
+$username = "sniffer";
+$password = "sniffer";
+$dbname = "traffic2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+
+if (isset($_GET["day"])){
+	echo "Report for day " . $_GET["day"] . "<br>";
+	
+	$sql = "select to_ip, round(sum(sum_size)/1000000000,1) as GB from reports where day=" . $_GET["day"] . " group by to_ip order by sum(sum_size) desc limit 10;";
+	$result = $conn->query($sql);
+
+	echo"<table border=1><tr><td>Dest IP</td><td>GB</td></tr>";
+	while($row = $result->fetch_assoc()) {
+			echo "<tr><td>" . $row["to_ip"]. "</td><td>" . $row["GB"] . "</td><td>" . str_repeat("l",$row["GB"]) . "</td></tr>";
+			}
+	echo"</table>";
+	}
+elseif (isset($_GET["today"])){
+	$sql = "select dayofyear(curdate()) as day";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$day = $row["day"];
+	echo "Report for today (" . $day . ")<br>";
+
+	$sql = "select to_ip, round(sum(size)/1000000000,1) as GB from data where day=" . $day . " group by to_ip order by sum(size) desc limit 10;";
+	$result = $conn->query($sql);
+
+	echo"<table border=1><tr><td>Dest IP</td><td>GB</td></tr>";
+	while($row = $result->fetch_assoc()) {
+			echo "<tr><td>" . $row["to_ip"]. "</td><td>" . $row["GB"] . "</td><td>" . str_repeat("l",$row["GB"]) . "</td></tr>";
+			}
+	echo"</table>";
+	}
+elseif (isset($_GET["first"])){
+	echo "Report for day " . $_GET["first"] . " to " . $_GET["last"] . "<br>";
+	
+	$sql = "select to_ip, round(sum(sum_size)/1000000000,1) as GB from reports where day>=" . $_GET["first"] . " and day<=" . $_GET["last"] . " group by to_ip order by sum(sum_size) desc limit 10;";
+	//echo $sql . "<br>";
+	$result = $conn->query($sql);
+
+	echo"<table border=1><tr><td>Dest IP</td><td>GB</td></tr>";
+	while($row = $result->fetch_assoc()) {
+			echo "<tr><td>" . $row["to_ip"]. "</td><td>" . $row["GB"] . "</td><td>" . str_repeat("l",$row["GB"]) . "</td></tr>";
+			}
+	echo"</table>";	
+	}
+else{
+	echo "<a href=old.php>Switch to old version</a><br><br>";
+	
+	echo "<form method=get>";
+	echo "Custom Range:<br>";
+	echo "First Day <input type=text name=first /></br>";
+	echo "Last Day <input type=text name=last /></br>";
+	echo "<input type=submit value=Submit />";
+	echo "</form><br>";
+	
+	echo "Daily Summary<br>";
+
+	$sql = "select day, round(sum(sum_size)/1000000000,1) as GB from reports group by day order by day desc;";
+	$result = $conn->query($sql);
+
+	echo"<table border=1><tr><td>DAY</td><td>GB</td></tr>";
+	echo"<tr><td>Today</td><td><a href=?today=1>Click</a></td></tr>";
+	while($row = $result->fetch_assoc()) {
+		echo "<tr><td><a href=/?day=" . $row["day"] . ">" . $row["day"]. "</a></td><td>" . $row["GB"] . "</td><td>" . str_repeat("l",$row["GB"]) . "</td></tr>";
+			}
+	echo"</table><br>";
+	}
+
+
+
+	?>
+  </body>
+</html>
+
