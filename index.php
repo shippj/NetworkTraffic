10d5902fb6accad3e7040e45a398950c8044e89b
shippj <github@nospam.jshipp.com>
2020-05-11 10:51:41 -0500

    Add files via upload
    shows IPs that used more than 50MB as opposed to the top10 IPs

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
@@ -12,7 +13,7 @@
 $servername = "localhost";
 $username = "sniffer";
 $password = "sniffer";
-$dbname = "traffic2";
+$dbname = "traffic";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -26,7 +27,7 @@ if ($conn->connect_error) {
 if (isset($_GET["day"])){
 	echo "Report for day " . $_GET["day"] . "<br>";
 	
-	$sql = "select to_ip, round(sum(sum_size)/1000000000,1) as GB from reports where day=" . $_GET["day"] . " group by to_ip order by sum(sum_size) desc limit 10;";
+	$sql = "select to_ip, round(sum(sum_size)/1000000000,1) as GB from reports where day=" . $_GET["day"] . " group by to_ip having sum(sum_size)>50000000 order by sum(sum_size) desc;";
 	$result = $conn->query($sql);
 
 	echo"<table border=1><tr><td>Dest IP</td><td>GB</td></tr>";
@@ -42,7 +43,7 @@ elseif (isset($_GET["today"])){
 	$day = $row["day"];
 	echo "Report for today (" . $day . ")<br>";
 
-	$sql = "select to_ip, round(sum(size)/1000000000,1) as GB from data where day=" . $day . " group by to_ip order by sum(size) desc limit 10;";
+	$sql = "select to_ip, round(sum(size)/1000000000,1) as GB from data where day=" . $day . " group by to_ip having sum(size)>50000000 order by sum(size) desc;";
 	$result = $conn->query($sql);
 
 	echo"<table border=1><tr><td>Dest IP</td><td>GB</td></tr>";
@@ -54,7 +55,7 @@ elseif (isset($_GET["today"])){
 elseif (isset($_GET["first"])){
 	echo "Report for day " . $_GET["first"] . " to " . $_GET["last"] . "<br>";
 	
-	$sql = "select to_ip, round(sum(sum_size)/1000000000,1) as GB from reports where day>=" . $_GET["first"] . " and day<=" . $_GET["last"] . " group by to_ip order by sum(sum_size) desc limit 10;";
+	$sql = "select to_ip, round(sum(sum_size)/1000000000,1) as GB from reports where day>=" . $_GET["first"] . " and day<=" . $_GET["last"] . " group by to_ip having sum(sum_size)>50000000 order by sum(sum_size) desc;";
 	//echo $sql . "<br>";
 	$result = $conn->query($sql);
 
@@ -65,8 +66,6 @@ elseif (isset($_GET["first"])){
 	echo"</table>";	
 	}
 else{
-	echo "<a href=old.php>Switch to old version</a><br><br>";
-	
 	echo "<form method=get>";
 	echo "Custom Range:<br>";
 	echo "First Day <input type=text name=first /></br>";
@@ -82,7 +81,7 @@ else{
 	echo"<table border=1><tr><td>DAY</td><td>GB</td></tr>";
 	echo"<tr><td>Today</td><td><a href=?today=1>Click</a></td></tr>";
 	while($row = $result->fetch_assoc()) {
-		echo "<tr><td><a href=/?day=" . $row["day"] . ">" . $row["day"]. "</a></td><td>" . $row["GB"] . "</td><td>" . str_repeat("l",$row["GB"]) . "</td></tr>";
+		echo "<tr><td><a href=?day=" . $row["day"] . ">" . $row["day"]. "</a></td><td>" . $row["GB"] . "</td><td>" . str_repeat("l",$row["GB"]) . "</td></tr>";
 			}
 	echo"</table><br>";
 	}
